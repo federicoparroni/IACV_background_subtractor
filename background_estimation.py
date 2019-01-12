cadba07811b8755f7dbe458a3a8b8291f6652b33
Federico Parroni <keyblade95@live.it>
2019-01-12 16:24:55 +0100

    Fixes

--- background_estimation.py
+++ background_estimation.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+import platform
+if platform.system() == 'Darwin':
+    import matplotlib
+    matplotlib.use('Qt4Agg')
+
 def background_estimate(str, show=True):
     cap = cv2.VideoCapture(str)
     V = []
@@ -18,13 +23,14 @@ def background_estimate(str, show=True):
 
     V = np.array(V)
     bg = np.zeros((V[0].shape[0], V[0].shape[1], 3), dtype=np.float32)
-    bg[:,:,0] = np.median(V[:,:,:,0], axis=0)/255
-    bg[:,:,1] = np.median(V[:,:,:,1], axis=0)/255
-    bg[:,:,2] = np.median(V[:,:,:,2], axis=0)/255
+    bg[:,:,0] = np.median(V[:,:,:,0], axis=0)
+    bg[:,:,1] = np.median(V[:,:,:,1], axis=0)
+    bg[:,:,2] = np.median(V[:,:,:,2], axis=0)
 
-    if show: plt.imshow(cv2.cvtColor(bg, cv2.COLOR_BGR2RGB))
-    if show: plt.show()
+    if show:
+        plt.imshow(cv2.cvtColor(bg/255, cv2.COLOR_BGR2RGB))
+        plt.show()
 
-    return bg*255
+    return bg
 
 # background_estimate('videos/svincolo.mp4', show=False)
--- background_subtraction.py
+++ background_subtraction.py
@@ -3,23 +3,29 @@ import cv2
 import numpy as np
 import time
 
-video = 'videos/traffic.mp4'
+video = 'videos/svincolo.mp4'
 bg = background_estimate(video, False)
 
 cap = cv2.VideoCapture(video)
-thr = 5
+threshold = 20
 while True:
     ret,frame = cap.read()
     if ret == False:
         break
     
-    mask = np.copy(frame, )
-    mask[np.abs(frame - bg) > 30] = 0
-    mask = np.product(mask, axis=2).astype(np.uint8)
+    mask = np.copy(frame)
+    mask[np.abs(frame - bg) > threshold] = 0
+    mask += 1
+    mask = np.product(mask, axis=2)
+    mask -= 1
     mask[mask>0]=255
-    frame = cv2.bitwise_and(frame, frame, mask = mask)
+    mask = mask.astype(np.uint8)
+
+    frame = cv2.bitwise_and(frame,frame, mask=mask)
 
     cv2.imshow('window-name',frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
     time.sleep(0.02)
 
 cap.release()
