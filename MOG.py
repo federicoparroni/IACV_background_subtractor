305c77c260569af4fca251d4848bbbb81aef518c
giovanni <giovanni.gabbolini@gmail.com>
2019-01-22 11:49:03 +0100

    skeleton for computation of metrics

--- MOG.py
+++ MOG.py
@@ -1,32 +1,68 @@
 import numpy as np
 import cv2 as cv
-from scipy.signal import convolve2d
+import os
+import time
+from tqdm import tqdm
 
-cap = cv.VideoCapture('videos/svincolo.mp4')
-# fgbg = cv.createBackgroundSubtractorMOG2()
-fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
-width = int(cap.get(3))
-height = int(cap.get(4))
-old_mask = np.ones((height, width))
-k = np.ones((3,3), np.uint8)
+class MOG():
 
-while(1):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    
-    fgmask = fgbg.apply(frame)
-    mask = np.logical_not(fgmask).astype(np.uint8)
+    def __init__(self):
+        self.fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
 
-    mask = mask.astype(np.uint8)
-    frame = cv.bitwise_and(frame, frame, mask=mask)
+    def process(self, frame):
+        fgmask = self.fgbg.apply(frame)
+        return fgmask
 
-    old_mask = np.copy(mask)
+    def demo(self, bp):
+        images = sorted(os.listdir(bp + 'input/'))
+        print('demo on ' + bp)
+        for i in tqdm(range(len(images))):
+            j = images[i]
+            image = cv.imread(bp + 'input/' + j)
+            mask = self.process(image)
+            mask = np.logical_not(mask).astype(np.uint8)
+            image = cv.bitwise_and(image, image, mask=mask)
+            cv.imshow('frame', image)
+            k = cv.waitKey(30) & 0xff
+            if k == 27:
+                break
 
-    cv.imshow('frame',frame)
-    k = cv.waitKey(30) & 0xff
-    if k == 27:
-        break
+    def evaluate(self, l):
+        for bp in l:
 
-cap.release()
-cv.destroyAllWindows()
+            images = sorted(os.listdir(bp + 'input/'))
+            gts = sorted(os.listdir(bp + 'groundtruth/'))
+            print('evaluating on ' + bp)
+            temporal_ROI = [int(x) for x in open(bp + 'temporalROI.txt').read().split(' ')]
+            tp = 0; fp = 0; tn = 0; fn = 0
+
+            for i in tqdm(range(len(images))):
+                j = images[i]
+                image = cv.imread(bp + 'input/' + j)
+                mask = self.process(image)
+
+                if i+1 >= temporal_ROI[0] and i+1 <= temporal_ROI[1]:
+                    k = gts[i]
+                    gt = cv.imread(bp + 'groundtruth/' + k)
+                    gt = np.all(gt, axis=2).astype(np.uint8)
+
+                    tp += np.sum(np.logical_and(gt,mask))
+                    tn += np.sum(np.logical_not(np.logical_or(gt,mask)))
+                    fn += np.sum(np.logical_and(gt, np.logical_xor(gt,mask)))
+                    fp += np.sum(np.logical_and(mask, np.logical_xor(gt,mask)).astype(np.uint8))
+
+                    # cv.imshow('Frame',mask)
+                    # if cv.waitKey(25) & 0xFF == ord('q'):
+                    #     print('eheh')
+            re = tp/(tp+fn)
+            pr = tp/(tp+fp)
+            f1 = (2*pr*re)/(pr+re)
+            sp = tn/(tn+fp)
+            fpr = fp/(fp+tn)
+            fnr = fn/(tn+fp)
+            pwc = 100*(fn+fp)/(tp+fn+fp+tn)
+            print('results: tp = {}, tn = {}, fp = {}, fn = {}. (avg) recall = {}, specificity = {}, false positive rate = {}, false negative rate = {}, percentage of wrong classification = {}, precision = {}, f1 score = {}'.format(tp, tn, fp, fn, re, sp, fpr, fnr, pwc, pr, f1))
+
+m = MOG()
+#m.demo('dataset/dataset/baseline/highway/')
+m.evaluate(['dataset/dataset/baseline/highway/'])
