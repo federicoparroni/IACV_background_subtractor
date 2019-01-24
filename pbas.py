c6066c970f9a02307fd396f08fe60f52b319cc5f
Federico Parroni <keyblade95@live.it>
2019-01-24 17:35:56 +0100

    Added some class properties

--- pbas.py
+++ pbas.py
@@ -17,12 +17,24 @@ class PBAS():
         self.T_lower = T_lower
         self.T_upper = T_upper
 
+        self.frame_shape = None
         self.current_frame_index = 0
         self.B = None
-        self.fg_mask = None
+        self.R = None
+        self.T = None
+        self._fg_mask = None
 
     def _segment(self, frame):
-        pass
+        if K > self.current_frame_index:
+            return self._fg_mask
+
+        for x in range(self.frame_shape[0]):
+            for y in range(self.frame_shape[1]):
+                for c in range(3):
+                    j = 0
+                    k = 0
+                    while j < min(N, self.current_frame_index) or k >= K:
+                        if self._distance() < 
 
     def _bgupdate(self, frame):
         pass
@@ -35,10 +47,18 @@ class PBAS():
 
 
     def process(self, frame):
+        if self.frame_shape is None:
+            self.frame_shape = frame.shape
         if self.B is None:
             self.B = np.zeros(frame.shape, np.uint8)
+        if self.R is None:
+            self.R = np.zeros(frame.shape, np.float)
+        if self.T is None:
+            self.T = np.zeros(frame.shape, np.float)
+        if self.fg_mask is None:
+            self.fg_mask = np.zeros(frame.shape, np.uint8)
 
-        self.fg_mask = self._segment(frame)
+        self._fg_mask = self._segment(frame)
         self._bgupdate(frame)
         self._updateR(frame)
         self._updateT(frame)
