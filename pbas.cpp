1fccb8a62fee470b7358a93710a44b7f72f74444
Federico Parroni <keyblade95@live.it>
2019-01-26 18:52:10 +0100

    ora sto bene

--- pbas.cpp
+++ pbas.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-
+#include <math.h>
 #include "pbas.h"
 
 using namespace std;
 
 
-PBAS::PBAS()
-{
+PBAS::PBAS() {
     cout << "Ciao!" << endl;
 }
 PBAS::PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, float T_dec=0.05, int T_inc=1, int T_lower=2, int T_upper=200)
@@ -14,9 +13,47 @@ PBAS::PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, f
     cout << "Ciao!" << endl;
 }
 
-PBAS::~PBAS()
-{
+PBAS::~PBAS() {}
+
+float PBAS::distance(int a, int b) {
+    return abs(a-b);
 }
+
+void updateF(Mat frame, int x, int y) {
+    //c = 0
+    //while c < 3 or k >= self.K:
+    int k = 0;       // number of lower-than-R distances for the channel 'c'
+    int j = 0;
+    while(j < N || k >= K) {
+        if(_distance(frame[x,y], B_copy[j,x,y]) < R_copy[x,y]) {
+            k++;
+        }
+        j++;
+    }
+    // check if at least K distances are less than R(x,y)
+    if(k >= K) {
+        F[x,y] = 1;
+    } else {
+        F[x, y] = 0;
+        updateBg(frame, x, y);
+    }
+}
+
+Mat* process(Mat* frame) {
+    w = frame->size().width();
+    h = frame->size().height();
+
+    for(int x = 0; x < w; x++)
+        for(int y = 0; y < h; y++)
+        {
+            updateF(frame, x,y);
+            updateR(frame, x,y);
+            updateT(frame, x,y);
+        }
+    
+    return F;
+}
+
 
 int main() {
     PBAS *p = new PBAS();
--- pbas.h
+++ pbas.h
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 class PBAS
 {
@@ -18,15 +19,24 @@ class PBAS
 
         int w;
         int h;
-        vector<cv::Mat> B;
-        vector<int> R;
-        vector<cv::Mat> D;
-        vector<int> T;
-        vector<int> F;
+        vector<Mat> B;
+        Mat R;
+        vector<Mat> D;
+        Mat T;
+        Mat F;
         float d_minavg;
 
+        float distance(int, int);
+
+        void updateF(Mat* frame, int x, int y);
+        void updateB(Mat* frame, int x, int y);
+        void updateR();
+        void updateT();
     public:
         PBAS();
         PBAS(int N, int K, float R_incdec, int R_lower, int R_scale, float T_dec, int T_inc, int T_lower, int T_upper);
         ~PBAS();
+
+        Mat* process(Mat);
+
 };
