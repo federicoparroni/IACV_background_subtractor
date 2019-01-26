da97152720de3237e0017b380c1178153ec3c2c0
Federico Parroni <keyblade95@live.it>
2019-01-26 14:56:37 +0100

    C++ version

---
+++ pbas.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+
+class PBAS
+{
+    private:
+        int N;
+        int K;
+        float R_incdec;
+        int R_lower;
+        int R_scale;
+        float T_dec;
+        int T_inc;
+        int T_lower;
+        int T_upper;
+
+    public:
+        PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, float T_dec=0.05, int T_inc=1, int T_lower=2, int T_upper=200);
+        ~PBAS();
+};
+
+PBAS::PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, float T_dec=0.05, int T_inc=1, int T_lower=2, int T_upper=200)
+{
+}
+
+PBAS::~PBAS()
+{
+}
