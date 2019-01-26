4fed46ab03cad9305f5160fd953faabe20592b69
Federico Parroni <keyblade95@live.it>
2019-01-26 14:57:44 +0100

    Added print

--- pbas.cpp
+++ pbas.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 using namespace std;
 
 class PBAS
@@ -20,6 +21,7 @@ class PBAS
 
 PBAS::PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, float T_dec=0.05, int T_inc=1, int T_lower=2, int T_upper=200)
 {
+    cout << 'Ciao!\n';
 }
 
 PBAS::~PBAS()
