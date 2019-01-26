f943990235d2c1798af9074d42f4cefcf294b3b0
Unknown <damicoedoardo@virgilio.it>
2019-01-26 15:16:27 +0100

    pbas c++ working

--- pbas.cpp
+++ pbas.cpp
@@ -15,7 +15,7 @@ class PBAS
         int T_upper;
 
     public:
-        PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, float T_dec=0.05, int T_inc=1, int T_lower=2, int T_upper=200);
+        PBAS(int N, int K, float R_incdec, int R_lower, int R_scale, float T_dec, int T_inc, int T_lower, int T_upper);
         ~PBAS();
 };
 
@@ -26,4 +26,9 @@ PBAS::PBAS(int N, int K=2, float R_incdec=0.05, int R_lower=18, int R_scale=5, f
 
 PBAS::~PBAS()
 {
+}
+
+int main()
+{
+  cout << "Hello World!\n";
 }
