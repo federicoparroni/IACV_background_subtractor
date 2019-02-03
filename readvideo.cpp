38216da12e630fd5d30de40ba8dc91b85b0ec807
Federico Parroni <keyblade95@live.it>
2019-02-03 17:37:20 +0100

    Old changes

--- readvideo.cpp
+++ readvideo.cpp
@@ -23,6 +23,8 @@ int main(int argc, char const *argv[]) {
         cap >> frame;
         if (frame.empty()) break;
 
+        //GaussianBlur(frame, frame, Size(3,3), 0, 0);
+
         mask = pbas->process(&frame);
         imshow("Frame", frame);
         moveWindow("Frame", 20,20);
@@ -30,14 +32,34 @@ int main(int argc, char const *argv[]) {
         imshow("Mask", *mask);
         moveWindow("Mask", 400,20);
 
-        imshow("Shadows CNCC", pbas->shadow_cncc);
-        moveWindow("Shadows CNCC", 750,20);
+        // Mat masked;
+        vector<Point2f> features;
+        //bitwise_and(frame,frame,masked,*mask);
+        // goodFeaturesToTrack(pbas->frame, features, 50, 0.8, 1, *mask);
+        // for(int i = 0; i<features.size(); ++i)
+        //     circle(frame, features[i], 4, Scalar(255,0,0), -1, 8, 0);
+        imshow("Masked", frame);
+        moveWindow("Masked", 750,20);
+
+
+        // imshow("Shadows CNCC", pbas->shadow_cncc);
+        // moveWindow("Shadows CNCC", 750,20);
 
         imshow("shadow_hsv", pbas->F_shadow_hsv);
-        moveWindow("Shadows hsv", 350,400);
+        moveWindow("Shadows hsv", 20,400);
 
-        imshow("shadow_corners", pbas->shadow_corner);
-        moveWindow("Shadows hsv", 350,400);
+        // Mat converted;
+        // pbas->median.convertTo(converted, CV_16SC3);
+        // frame.convertTo(frame, CV_16SC3);
+        // Mat res = abs(frame - converted);
+        // res.convertTo(res, CV_8UC3);
+        // cvtColor(res, res, COLOR_RGB2GRAY);
+        // threshold(res, res, 50, 255, THRESH_BINARY);
+        // imshow("bg model", res);
+        // moveWindow("bg model", 350,400);
+
+        // imshow("shadow_corners", pbas->shadow_corner);
+        // moveWindow("Shadows hsv", 350,400);
 
         // Canny(pbas->frame, edges, 80, 200);
         // imshow("Canny", edges);
