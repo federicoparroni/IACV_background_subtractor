68613bd1a71dd6f88e044033d44d939afd688d52
Federico Parroni <keyblade95@live.it>
2019-02-06 17:17:04 +0100

    Video chunks

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,20 +1,23 @@
 import pafy
 import cv2
 import os
+import math
 # import time
 
 #url = "https://www.youtube.com/watch?v=Zwt3ZOej3_U"
-DOWNLOAD_FOLDER = 'dataset'
-SAVE_VIDEO = True   # set to false to save single frames
-FPS = 20
+DOWNLOAD_FOLDER = 'dataset'         # download folder
+SAVE_VIDEO = True                   # set to false to save single frames
+FPS = 30
+VIDEO_CODEC = cv2.VideoWriter_fourcc(*'mp4v')
+VIDEO_CHUNKS_FRAMES = 30*(FPS*60)    # save video at chunks of frames (30 min @ 25 fps)
 
 url = "https://www.youtube.com/watch?v=1EiC9bvVGnk"
 video = pafy.new(url)
-print("- Info:")
+print('> Info:')
 print(video)
 
 # best = video.getbest(preftype="mp4")
-print("- Streams:")
+print("> Streams:")
 streams = [s for s in video.streams if s.extension == 'mp4']
 index = 0
 for s in streams:
@@ -32,29 +35,60 @@ video_folder = "{}/{}".format(DOWNLOAD_FOLDER, video.title[0:filename_max_length
 if not os.path.isdir(video_folder):
     os.makedirs(video_folder)
 
-if SAVE_VIDEO:
-    output_codec = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('{}/out.mov'.format(video_folder), output_codec, FPS, stream.dimensions)
+def get_frame(capture):
+    ret, frame = capture.read()
+    cv2.imshow('yt', frame)
+    return ret, frame
 
-cap = cv2.VideoCapture()
-cap.open(videourl)
+def print_progress(current, tot, prefix=''):
+    scale = 30 / tot
+    left = math.floor((tot-current) * scale)
+    curr = math.ceil(current * scale)
+    print('{}: |={}{}|'.format(prefix, '='*curr, ' '*left), end='\r')
+
+def print_minutage(frames_count):
+    tot_min = frames_count / (FPS * 60)
+    print('Frame {} - Tot duration: {}min'.format(frames_count, tot_min))
+
+shape = stream.dimensions[::-1]
+if SAVE_VIDEO:
+    cap = None
+else:
+    cap = cv2.VideoCapture()
+    cap.open(videourl)
 
 ret = True
-i = 0
-while cap.isOpened() and ret:
-    ret, frame = cap.read()
-
-    cv2.imshow('yt', frame)
-
+frames_count = 0
+video_chunk = 0
+while True:
     if SAVE_VIDEO:
-        #cv2.flip(frame,0)
-        out.write(frame)
-    else:
-        framename = '{}/frame{:016d}.jpg'.format(video_folder, i)
-        cv2.imwrite(framename, frame)
-        i += 1
+        current_frame = frames_count % VIDEO_CHUNKS_FRAMES
+        # check if a new chunk must be created
+        if current_frame == 0:
+            if cap is None:
+                cap = cv2.VideoCapture()
+                cap.open(videourl)
+            else:    # release the previous video session
+                out.release()
+            out = cv2.VideoWriter('{}/out{}.mov'.format(video_folder, video_chunk), VIDEO_CODEC, FPS, stream.dimensions)
+            video_chunk += 1
+            print()
+        
+        ret, frame = get_frame(cap)
+        if frame.shape[:2] == shape:
+            out.write(frame)
 
+        print_progress(current_frame, VIDEO_CHUNKS_FRAMES, video_chunk)
+    else:   # save single frames
+        ret, frame = get_frame(cap)
+        framename = '{}/frame{:016d}.jpg'.format(video_folder, frames_count)
+        cv2.imwrite(framename, frame)
+        
+        print_minutage(frames_count)
+
+    frames_count += 1
     if cv2.waitKey(10) & 0xFF == ord('q'): break
+    if not cap.isOpened() or ret == False: break
 
 if SAVE_VIDEO: out.release()
 cap.release()
