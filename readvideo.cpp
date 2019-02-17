798335a28336b43885e5627989b055a8efa6fcab
giovanni <giovanni.gabbolini@gmail.com>
2019-02-17 12:29:24 +0100

    various paper things + default parameters on the algorithm

--- readvideo.cpp
+++ readvideo.cpp
@@ -99,12 +99,12 @@ int main(int argc, char const *argv[]) {
     int K = 3;
     float R_incdec = 0.05;
     int R_lower = 18;
-    int R_scale = 5;
+    int R_scale = 2;
     float T_dec = 0.05;
-    int T_inc = 1;
+    int T_inc = 5;
     int T_lower = 2;
     int T_upper = 200;
-    int alpha = 10;
+    int alpha = 20;
     if(argc == 12) {
         N = stoi(argv[2]);
         K = stoi(argv[3]);
